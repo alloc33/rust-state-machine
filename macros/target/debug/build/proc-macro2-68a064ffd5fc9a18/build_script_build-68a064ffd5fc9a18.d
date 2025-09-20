a7f762ccb80f5540b81b6bc0f5bdd58edbe05799
Nikolai Shelekhov <nickshv13@icloud.com>
2025-09-20 14:10:17 +0700

    c

--- macros/target/debug/build/proc-macro2-68a064ffd5fc9a18/build_script_build-68a064ffd5fc9a18.d
+++
@@ -1,5 +0,0 @@
-/Users/nshv/Repos/rust-state-machine/macros/target/debug/build/proc-macro2-68a064ffd5fc9a18/build_script_build-68a064ffd5fc9a18.d: /Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.101/build.rs
-
-/Users/nshv/Repos/rust-state-machine/macros/target/debug/build/proc-macro2-68a064ffd5fc9a18/build_script_build-68a064ffd5fc9a18: /Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.101/build.rs
-
-/Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.101/build.rs:
--- macros/target/debug/deps/unicode_ident-3c4781cfc588c13e.d
+++
@@ -1,6 +0,0 @@
-/Users/nshv/Repos/rust-state-machine/macros/target/debug/deps/unicode_ident-3c4781cfc588c13e.d: /Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/lib.rs /Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/tables.rs
-
-/Users/nshv/Repos/rust-state-machine/macros/target/debug/deps/libunicode_ident-3c4781cfc588c13e.rmeta: /Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/lib.rs /Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/tables.rs
-
-/Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/lib.rs:
-/Users/nshv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/tables.rs:
